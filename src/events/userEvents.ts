7ab6ccf76743668e24cfd126dd99f569c6eb8172
John Barry <john.olivierb@gmail.com>
2023-06-09 19:12:50 -0400

    Update: Cleanup functions and optimization

--- src/events/userEvents.ts
+++ src/events/userEvents.ts
@@ -1,22 +1,17 @@
 import { Server, Socket } from 'socket.io';
 import supabase from '../supabase';
 import { selectUserChampion } from '../utils/champions';
-import { switchTurnAndUpdateCycle } from '../utils/roomCycle';
-import { updateRoomCycle } from '../utils/roomCycle';
-import { initTimer, startTimer } from '../utils/timer';
+import { switchTurnAndUpdateCycle, updateRoomCycle } from '../utils/roomCycle';
+import { resetTimer, startLobbyTimer } from '../utils/timer';
 
 export const handleUserEvents = (socket: Socket, io: Server) => {
   socket.on('SELECT_CHAMPION', async (data) => {
-    try {
-      const { roomid, selectedChampion } = data;
-      await selectUserChampion(roomid, selectedChampion);
-      await switchTurnAndUpdateCycle(roomid);
-      io.to(roomid).emit('message', `User ${socket.id} has selected ${selectedChampion}`);
-    } catch (error: any) {
-      console.error(`Error handling SELECT_CHAMPION event: ${error.message}`);
-    }
+    const { roomid, selectedChampion } = data;
+    await selectUserChampion(roomid, selectedChampion);
+    await switchTurnAndUpdateCycle(roomid);
+    resetTimer(roomid);
+    io.to(roomid).emit('message', `User ${socket.id} has selected ${selectedChampion}`);
   });
-
 
   socket.on('TEAM_READY', async ({ roomid, teamid }) => {
     const { data: teams } = await supabase
@@ -26,15 +21,13 @@ export const handleUserEvents = (socket: Socket, io: Server) => {
 
     if(!teams) return;
     
-    console.log(`Team ${teamid} is ready!`)
+    console.log(`Team ${teamid} is ready!`);
 
     if (teams.every(team => team.ready)) {
       await supabase.from('rooms').update({ ready: true }).eq('id', roomid);
-
-      startTimer(roomid);
+      console.log(`Room ${roomid} is ready!`);
+      startLobbyTimer(roomid);
       updateRoomCycle(roomid);
-     // io.in(roomid).emit('ROOM_READY', 'Room is ready to start!');
-      
     }
   });
 };
