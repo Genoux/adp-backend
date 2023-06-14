a7c7422f225b91af5ccb7e5a50649bff93d3e66a
John Barry <john.olivierb@gmail.com>
2023-06-13 22:01:57 -0400

    Cleanup

--- src/events/userEvents.ts
+++ src/events/userEvents.ts
@@ -1,52 +1,56 @@
-import { Server, Socket } from 'socket.io';
-import supabase from '../supabase';
-import { selectUserChampion } from '../utils/champions';
-import { updateRoomCycle } from '../utils/roomCycle';
-import { switchTurn } from '../utils/switchTeam';
-import { resetTimer, startLobbyTimer, stopTimer, setHeroSelected } from '../utils/timer';
+import { Server, Socket } from "socket.io";
+import supabase from "../supabase";
+import { selectUserChampion } from "../utils/champions";
+import { updateRoomCycle } from "../utils/roomCycle";
+import { switchTurn } from "../utils/switchTeam";
+import {
+  resetTimer,
+  startLobbyTimer,
+  stopTimer,
+  setHeroSelected,
+} from "../utils/timer";
 
 export const handleUserEvents = (socket: Socket, io: Server) => {
-  socket.on('SELECT_CHAMPION', async (data) => {
-    const { roomid, selectedChampion } = data;
-    //stopTimer(roomid);
+  socket.on("SELECT_CHAMPION", async ({ roomid, selectedChampion }) => {
     await setHeroSelected(roomid, true);
     await selectUserChampion(roomid, selectedChampion);
-    io.to(roomid).emit('message', `User ${socket.id} has selected ${selectedChampion}`);
+    io.to(roomid).emit(
+      "message",
+      `User ${socket.id} has selected ${selectedChampion}`
+    );
     const cycle = await updateRoomCycle(roomid);
-    const value = await switchTurn(roomid, cycle);
-    console.log("socket.on - value:", value);
-    if (!value) {
-      socket.emit('CHAMPION_SELECTED', true);
+    if (!await switchTurn(roomid, cycle)) {
+      socket.emit("CHAMPION_SELECTED", true);
     }
     resetTimer(roomid);
-    socket.to(roomid).emit('TIMER_RESET', true);
+    socket.to(roomid).emit("TIMER_RESET", true);
   });
 
-  socket.on('RESET_TIMER', async (data) => {
-    console.log("Stop timer for: ", data);
-    resetTimer(data.roomid);
+  socket.on("RESET_TIMER", ({ roomid }) => {
+    console.log("Reset timer for: ", roomid);
+    resetTimer(roomid);
   });
 
-  socket.on('STOP_TIMER', async (data) => {
-    console.log("Stop timer for: ", data);
-    stopTimer(data.roomid);
+  socket.on("STOP_TIMER", ({ roomid }) => {
+    console.log("Stop timer for: ", roomid);
+    stopTimer(roomid);
   });
 
-  socket.on('TEAM_READY', async ({ roomid, teamid }) => {
+  socket.on("TEAM_READY", async ({ roomid, teamid }) => {
     const { data: teams } = await supabase
-      .from('teams')
-      .select('*')
-      .eq('room', roomid);
+      .from("teams")
+      .select("*")
+      .eq("room", roomid);
 
-    if(!teams) return;
-    
+    if (!teams) return;
+
     console.log(`Team ${teamid} is ready!`);
 
-    if (teams.every(team => team.ready)) {
-      await supabase.from('rooms').update({ ready: true }).eq('id', roomid);
+    if (teams.every((team) => team.ready)) {
+      await supabase.from("rooms").update({ ready: true }).eq("id", roomid);
       console.log(`Room ${roomid} is ready!`);
       startLobbyTimer(roomid);
-      updateRoomCycle(roomid);
+      await updateRoomCycle(roomid);
     }
   });
 };
