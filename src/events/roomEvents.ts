ec0ba8d294b3cb424fab2c50d18915b07dddbcb3
John Barry <john.olivierb@gmail.com>
2023-08-30 10:18:54 -0400

    Handle multiple connection
    Handle multiple connection. If on quit but there's still a connection, connected is still true

--- src/events/roomEvents.ts
+++ src/events/roomEvents.ts
@@ -11,9 +11,16 @@ export const handleRoomEvents = (socket: Socket, io: Server) => {
     console.dir(`User ${socket.id} joined room ${roomid} as Team ${teamid}`);
 
     //TODO Handle multiple connection. If on quit but there's still a connection, connected is still true
-   setTimeout(async () => {
-    await supabase.from("teams").update({ connected: true, socketid: socket.id }).eq("id", teamid)
-   }, 1000);
+  //  setTimeout(async () => {
+  //   await supabase.from("teams").update({ connected: true, socketid: socket.id }).eq("id", teamid)
+    //  }, 1000);
+    setTimeout(async () => {
+      const { data: team } = await supabase.from("teams").select("socketid").eq("id", teamid).single();
+      const existingSocketIds = team?.socketid ? team.socketid : [];
+      const newSocketIds = [...existingSocketIds, socket.id];
+      socket.data.teamid = teamid;
+      await supabase.from("teams").update({ connected: true, socketid: newSocketIds }).eq("id", teamid);
+    }, 1000);
 
     socket.emit("message", `Welcome to room ${roomid}, Team ${teamid}!`);
 
@@ -38,6 +45,13 @@ export const handleRoomEvents = (socket: Socket, io: Server) => {
 
   socket.on("disconnect", async (reason) => {
     console.log(`User ${socket.id} disconnected because ${reason}`);
-    await supabase.from("teams").update({ connected: false, socketid: null }).eq("socketid", socket.id)
+    const teamid = socket.data.teamid;
+
+    const { data: team } = await supabase.from("teams").select("socketid").eq("id", teamid).single();
+    const existingSocketIds = team?.socketid ? team.socketid : [];
+    const newSocketIds = existingSocketIds.filter((id: string) => id !== socket.id);
+    console.log(newSocketIds);
+    const isConnected = newSocketIds.length > 0;
+    await supabase.from("teams").update({ connected: isConnected, socketid: newSocketIds }).eq("id", teamid);
   });
 };
