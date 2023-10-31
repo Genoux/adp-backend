a3437f366804e4292e8d8ad109a7b56ec6bc54cc
John Barry <john.olivierb@gmail.com>
2023-10-31 15:09:24 -0400

    Optimize champion selection script

--- src/utils/champions.ts
+++ src/utils/champions.ts
@@ -1,16 +1,9 @@
 import supabase from "../supabase";
 
-type Hero = { 
+type Hero = {
   id: string;
-  name: string | null; 
+  name: string | null;
   selected?: boolean;
-};
-
-type Team = {
-  id: number;
-  heroes_selected: { id: string; name: string; selected?: boolean }[];
-  heroes_ban: { id: string; name: string; selected?: boolean }[];
-  clicked_hero: string;
 };
 
 const actionFunctionMapping = {
@@ -18,96 +11,53 @@ const actionFunctionMapping = {
   select: "heroes_selected",
 };
 
-async function selectChampion(
-  roomid: string,
-  selectedChampion: string | null
-) {
-  const { data: team } = await supabase
-    .from("teams")
-    .select(
-      "id, isturn, heroes_selected, heroes_ban, clicked_hero, nb_turn"
-    )
-    .eq("room", roomid).eq("isturn", true).single();
+const getHeroFromPool = (heroes_pool: Hero[], heroName: string | null) => {
+  const hero = heroName ? heroes_pool.find(h => h.name === heroName) : undefined;
+  if (hero?.selected) throw new Error("Hero already selected");
+  return hero;
+};
+
+const updateHeroSelectionInPool = (heroes_pool: Hero[], hero?: Hero) =>
+  heroes_pool.map(h => (h.name === hero?.name ? { ...h, selected: true } : h));
+
+const updateTeamHeroSelection = (heroes: Hero[], hero?: Hero) => {
+  const index = heroes.findIndex(h => !h.selected);
+  if (index !== -1) heroes[index] = { id: hero?.id || '', name: hero?.name || null, selected: true };
+};
+
+async function selectChampion(roomid: string, selectedChampion: string | null) {
+  const teamResponse = await supabase.from('teams').select('id, isturn, heroes_selected, heroes_ban, clicked_hero, nb_turn').eq('room', roomid).eq('isturn', true).single();
+  const roomResponse = await supabase.from('rooms').select('id, status, heroes_pool').eq('id', roomid).single();
+
+  if (!teamResponse.data || !roomResponse.data) return;
+  const team = teamResponse.data;
+  const room = roomResponse.data;
+
+  const action = room.status === 'ban' ? 'ban' : 'select';
+  const heroesAction = action === 'ban' ? team.heroes_ban : team.heroes_selected;
+
+  // Update clicked_hero to null right after retrieving the data
+  await supabase.from('teams').update({ clicked_hero: null }).eq('id', team.id);
+
+  // If action is 'select' and no champion is selected, pick a random one
+  const hero = selectedChampion ? getHeroFromPool(room.heroes_pool, selectedChampion) : (action === 'select' ? getRandomUnselectedHero(room.heroes_pool) : undefined);
   
-  const { data: room } = await supabase
-      .from("rooms")
-      .select("id, status, heroes_pool")
-      .eq("id", roomid)
-      .single();
-  
-    if (!team || !room ) return;
-  
-  await supabase.from("teams").update({ clicked_hero: null }).eq("id", team.id);
+  // Update hero selection in the team
+  updateTeamHeroSelection(heroesAction, hero);
 
-  const typedTeam = team as unknown as Team
+  // Update heroes pool
+  const updatedHeroesPool = updateHeroSelectionInPool(room.heroes_pool, hero);
 
-  const action = room.status === "ban" ? "ban" : "select";
-  const heroes_action =
-    action === "ban" ? team.heroes_ban : team.heroes_selected;
+  // Update team in the database
+  await supabase.from('teams').update({ [actionFunctionMapping[action]]: heroesAction, nb_turn: team.nb_turn - 1 }).eq('id', team.id);
 
-  if (action !== "ban") {
-    selectedChampion = selectedChampion || typedTeam.clicked_hero;
-  }
-
-  let hero: Hero | undefined;
-  if (selectedChampion) {
-    hero = room.heroes_pool.find(
-      (hero: Hero) => hero.name === selectedChampion
-    );
-    if (hero?.selected) throw new Error("Hero already selected");
-  } else if (!selectedChampion && action === "select") {
-    hero = getRandomUnselectedHero(room.heroes_pool);
-    if (!hero) throw new Error("No unselected hero found");
-  }
-
-  if (action === "ban") {
-    const currentBanIndex = heroes_action.findIndex(
-      (hero: { id: string, name: string; selected?: boolean }) => !hero.selected
-    );
-    if (currentBanIndex !== -1) {
-      if (hero) {
-        heroes_action[currentBanIndex] = { id: hero.id, name: hero.name, selected: true };
-      } else {
-        heroes_action[currentBanIndex] = { id: null, name: null, selected: true };
-      }
-    }
-  } else if (hero) {
-    const nullSlotIndex = heroes_action.findIndex(
-      (hero: { id: string, name: string; selected?: boolean }) => !hero.selected
-    );
-    if (nullSlotIndex !== -1) {
-      heroes_action[nullSlotIndex] = { id: hero.id, name: hero.name, selected: true };
-    }
-  }
-
-  const updatedHeroesPool = room.heroes_pool.map((poolHero: Hero) =>
-    poolHero.name === hero?.name ? { ...poolHero, selected: true } : poolHero
-  );
-
-  await supabase
-    .from("teams")
-    .update({
-      [actionFunctionMapping[action]]: heroes_action.map(
-        ({ id, name, selected }: { id: string, name: string; selected?: boolean }) => ({
-          id,
-          name,
-          selected,
-        })
-      ),
-      nb_turn: team.nb_turn - 1 
-    })
-    .eq("id", team.id);
-
-  await supabase
-    .from("rooms")
-    .update({ heroes_pool: updatedHeroesPool })
-    .eq("id", roomid);
+  // Update room in the database
+  await supabase.from('rooms').update({ heroes_pool: updatedHeroesPool }).eq('id', roomid);
 }
 
 function getRandomUnselectedHero(heroes_pool: Hero[]): Hero | undefined {
   const unselectedHeroes = heroes_pool.filter((hero) => !hero.selected);
-  if (unselectedHeroes.length === 0) return;
-
+  if (unselectedHeroes.length === 0) return undefined;
   const randomIndex = Math.floor(Math.random() * unselectedHeroes.length);
   return unselectedHeroes[randomIndex];
 }
