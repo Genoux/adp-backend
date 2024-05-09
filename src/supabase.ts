a74b50e26aec4a71bd42146cbf41f802ee05a1ad
John Barry <john.olivierb@gmail.com>
2024-05-09 15:38:18 -0400

    Use schema ADP

--- src/supabase.ts
+++ src/supabase.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseKey = process.env.SUPABASE_ANON_KEY || '';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey,
+  { db: { schema: 'aram_draft_pick' } }
+);
 
 export default supabase;
