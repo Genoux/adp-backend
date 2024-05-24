4113c8ca6d2120fc073a8ad8efe5b0d2f828ba36
John Barry <john.olivierb@gmail.com>
2024-05-24 19:43:24 -0400

    If ban is empty display null

--- src/utils/heroes.ts
+++ src/utils/heroes.ts
@@ -2,10 +2,10 @@ type Hero = {
   id: string;
   name: string | null;
   selected?: boolean;
-};
+} | null;
 
 const getRandomUnselectedHero = (heroesPool: Hero[]): Hero | undefined => {
-  const unselectedHeroes = heroesPool.filter(hero => !hero.selected);
+  const unselectedHeroes = heroesPool.filter(hero => hero && !hero.selected);
   if (unselectedHeroes.length === 0) return undefined;
   const randomIndex = Math.floor(Math.random() * unselectedHeroes.length);
   return unselectedHeroes[randomIndex];
@@ -13,7 +13,7 @@ const getRandomUnselectedHero = (heroesPool: Hero[]): Hero | undefined => {
 
 const getHeroFromPool = (heroesPool: Hero[], heroName: string | null): Hero | undefined => {
   if (!heroName) return getRandomUnselectedHero(heroesPool);
-  const hero = heroesPool.find(h => h.name === heroName);
+  const hero = heroesPool.find(h => h && h.name === heroName);
   if (hero?.selected) {
     console.log("Hero already selected, returning random hero");
     return getRandomUnselectedHero(heroesPool);
@@ -22,10 +22,17 @@ const getHeroFromPool = (heroesPool: Hero[], heroName: string | null): Hero | un
 };
 
 const updateHeroSelectionInPool = (heroesPool: Hero[], hero?: Hero): Hero[] =>
-  heroesPool.map(h => h.name === hero?.name ? { ...h, selected: true } : h);
+  heroesPool.map(h => {
+    if (h === null) return null;
+    return {
+      ...h,
+      selected: h.name === hero?.name ? true : h.selected,
+      id: h.name === hero?.name ? (hero?.id || '') : h.id || '',
+    };
+  });
 
 const updateTeamHeroSelection = (heroes: Hero[], hero?: Hero): void => {
-  const index = heroes.findIndex(h => !h.selected);
+  const index = heroes.findIndex(h => h !== null && !h.selected);
   if (index !== -1) {
     heroes[index] = {
       id: hero?.id || '',
